f43f980d7bcbfc2e4a525d916d58a0aa2a935ba7
MIKHAIL <MIKHAIL@DESKTOP-6MU8OHN>
2022-11-27 23:36:20 +0500

    Добавьте файлы проекта.

---
+++ Задача_12_работа над ошибками/Circle.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Задача_12_работа_над_ошибками
+
+    internal static class Circle
+{
+    public static double GetLength(double r)
+    {
+        return 2 * Math.PI * r;
+    }
+    public static double GetArea(double r)
+    {
+        return Math.PI * r * r;
+    }
+    public static bool IsInCircle(double r, double x1, double y1, double x2, double y2)
+    {
+        return Math.Sqrt(Math.Pow((x1 - x2), 2) + Math.Pow((y1 - y2), 2)) <= r;
+    }
+}
+}
